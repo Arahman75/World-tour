72d06242a47d2f7f64a8fa2b0f34f287a3db7561
Arahman75 <selfarahman01825@gmail.com>
2022-02-11 16:10:11 +0600

    add do some information

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-// first step
-// fetch('https://restcountries.com/v2/all')
-// .then(res => res.json())
-// .then(data => displayCountries(data));
+//first step
+fetch('https://restcountries.com/v2/all')
+.then(res => res.json())
+.then(data => displayCountries(data));
 
-// const displayCountries = countries =>{
-//     const countriesDiv = document.getElementById('countries');
-//     for (let i = 0; i < countries.length; i++) {
-//         const country = countries[i];
-//         const countryDiv = document.createElement('div');
-//         const h3 = document.createElement('h3');
-//         h3.innerText = country.name;
-//         countryDiv.appendChild(h3);
+const displayCountries = countries =>{
+    const countriesDiv = document.getElementById('countries');
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        const countryDiv = document.createElement('div');
+        const h3 = document.createElement('h3');
+        h3.innerText = country.name;
+        countryDiv.appendChild(h3);
 
-//         const p = document.createElement('p');
-//         p.innerText = country.capital;
-//         countryDiv.appendChild(p);
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+        countryDiv.appendChild(p);
 
-//         countriesDiv.appendChild(countryDiv);    
-//     }
-// }
+        countriesDiv.appendChild(countryDiv);    
+    }
+}
 
 
 
 //second step
-// fetch('https://restcountries.com/v2/all')
-// .then(res => res.json())
-// .then(data => displayCountries(data));
+fetch('https://restcountries.com/v2/all')
+.then(res => res.json())
+.then(data => displayCountries(data));
 
-// const displayCountries = countries =>{
-//     const countriesDiv = document.getElementById('countries');
-//     for (let i = 0; i < countries.length; i++) {
-//         const country = countries[i];
-//         const countryDiv = document.createElement('div');
-//         countryDiv.className = 'country';
-//         const countryInfo =`
-//         <h3 class="country-name">${country.name}</h3>
-//         <p>${country.capital}</p>
-//         `
-//         countryDiv.innerHTML = countryInfo;
-//         countriesDiv.appendChild(countryDiv);    
-//     }
-// }
+const displayCountries = countries =>{
+    const countriesDiv = document.getElementById('countries');
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        const countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
+        const countryInfo =`
+        <h3 class="country-name">${country.name}</h3>
+        <p>${country.capital}</p>
+        `
+        countryDiv.innerHTML = countryInfo;
+        countriesDiv.appendChild(countryDiv);    
+    }
+}
 
 
 //third step
